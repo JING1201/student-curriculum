d92d0941f8da42b9dc08592501f3ec74a080513e
Kim Huynh <kimr843@gmail.com>
2017-03-07 19:39:33 +0000

    Add particles foreground

---
+++ js/main.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+  Particles.init({ options });
+};
+
+// e.g.
+window.onload = function() {
+  Particles.init({
+    selector: '#myCanvas',
+    color: '#0f9976',
+    connectParticles: true
+  });
+};
