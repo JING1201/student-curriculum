6c33d7d6c9959cd48a197f1d6cfcdd68060b5896
Kim Huynh <kimr843@gmail.com>
2017-03-09 08:09:40 +0000

    Add include file: scripts

--- include/header.php
+++ include/header.php
@@ -1,7 +1,7 @@
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
+<!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">-->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 
---
+++ include/scripts.php
@@ -0,0 +1,4 @@
+<script src="https://npmcdn.com/particlesjs@1.0.3/dist/particles.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="js/main.js"></script>
