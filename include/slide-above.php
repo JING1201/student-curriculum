9b3965391dab68744ea807859874ea62a1e1406d
Kim Huynh <kimr843@gmail.com>
2017-03-12 04:49:56 +0000

    Change slide include files

--- include/slide-above.php
+++ include/slide-above.php
@@ -1,11 +1,25 @@
 <!doctype html>
 <html>
-<?php include 'include/header.php';?> 
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">-->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+
+<link rel="stylesheet" href="../css/main.css">
+</head>
 
 <body>
   
 <div class="container">
-<?php include 'include/navbar.php';?> 
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <a class="navbar-brand" href="#"><h1></h1></a>
+    <h3 class="navbar-text pull-left"><a href="../index.html" class="navbar-link">UF TYPE</a></h3>
+    <h5 class="navbar-text pull-right"><a href="https://jsfiddle.net/" class="navbar-link">JS Fiddle</a></h5>
+  </div>
+</nav>
 
   <!-- Slide grid -->
   <div class="slide-grid container">
