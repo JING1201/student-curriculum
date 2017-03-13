dfab5cb164dbc38b199044bc7c87349c7624552f
Kim Huynh <kimr843@gmail.com>
2017-03-13 14:03:21 +0000

    Add js fiddle button

--- include/slide-above.php
+++ include/slide-above.php
@@ -17,7 +17,7 @@
   <div class="container">
     <a class="navbar-brand" href="#"><h1></h1></a>
     <h3 class="navbar-text pull-left"><a href="../index.html" class="navbar-link">UF TYPE</a></h3>
-    <h5 class="navbar-text pull-right"><a href="https://jsfiddle.net/" class="navbar-link">JS Fiddle</a></h5>
+    <h5 class="navbar-text pull-right"><a href="https://jsfiddle.net/" class="js-fiddle">JS Fiddle</a></h5>
   </div>
 </nav>
 
