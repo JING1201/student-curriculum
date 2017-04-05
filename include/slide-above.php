41ad5297f17a5866a0052285443003abbaf53330
Kim Huynh <kimr843@gmail.com>
2017-04-05 12:29:45 +0000

    Update slide include - for modal

--- include/slide-above.php
+++ include/slide-above.php
@@ -1,26 +1,11 @@
-<!doctype html>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- Modal -->
+<div id="zero" class="modal fade" role="dialog">
+  <div class="modal-dialog">
 
-<!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">-->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-
-<link rel="stylesheet" href="../css/main.css">
-</head>
-
-<body>
-  
-<div class="container">
-<nav class="navbar navbar-default navbar-fixed-top">
-  <div class="container">
-    <a class="navbar-brand" href="#"><h1></h1></a>
-    <h3 class="navbar-text pull-left"><a href="../index.html" class="navbar-link">UF TYPE</a></h3>
-    <h5 class="navbar-text pull-right"><a href="https://jsfiddle.net/" class="js-fiddle">JS Fiddle</a></h5>
-  </div>
-</nav>
-
-  <!-- Slide grid -->
-  <div class="slide-grid container">
-    <div class="slide row">
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">0. Intro to JS Fiddle</h4>
+      </div>
+      <div class="modal-body">
--- include/slide-below.php
+++ include/slide-below.php
@@ -1,10 +1,9 @@
-  </div>
-  </div>
 
-<script src="https://npmcdn.com/particlesjs@1.0.3/dist/particles.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<script src="../js/main.js"></script>
-  
-</body>
-</html>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
