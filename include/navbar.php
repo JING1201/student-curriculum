e20ec7d07644283079db95deed41d00b7eb6fd66
Kim Huynh <kimr843@gmail.com>
2017-08-20 03:17:49 +0000

    clean files

--- include/navbar.php
+++
@@ -1,7 +0,0 @@
-<!--<nav class="navbar navbar-default navbar-fixed-top">-->
-<!--  <div class="container">-->
-<!--    <a class="navbar-brand" href="#"><h1></h1></a>-->
-<!--    <h3 class="navbar-text"><a href="index.html" class="navbar-link">UF TYPE</a></h3>-->
-<!--    <h5 class="navbar-text"><a href="https://jsfiddle.net/" class="js-fiddle">JS Fiddle</a></h5>-->
-<!--  </div>-->
-<!--</nav>-->
