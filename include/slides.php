5c63d823e1da87b1acaefb4526945954c6f2332b
Kim Huynh <kimr843@gmail.com>
2017-08-23 01:19:12 +0000

    rewrite slide embed

--- include/slides.php
+++ include/slides.php
@@ -1,14 +1,14 @@
 <?php 
 $count = 0;
 $a = [
-  "0. Intro to JS Fiddle" => "https://docs.google.com/presentation/d/170XzoPLJkZcYjhpHoz5lFDymTsjKa9-rFK0CPi0-wmI/embed?start=false&loop=false&delayms=3000",
-  "1. Variables" => "https://docs.google.com/presentation/d/1yFbB36iDzILW6oibqwO8ZA44keUjIk40flAsR0c77P0/embed?start=false&loop=false&delayms=3000",
-  "2. Conditionals" => "https://docs.google.com/presentation/d/1DXWCMmM1LPjFbFcQ_yi55zyUUUjQnZiIF23oaSndnPQ/embed?start=false&loop=false&delayms=3000",
-  "3. While Loops" => "https://docs.google.com/presentation/d/1n-QKn0nCXn_O-yz7fe_0_Y9eda2XApKQpEs9Om3YKG4/embed?start=false&loop=false&delayms=3000",
-  "4. For Loops" => "https://docs.google.com/presentation/d/1Hzf15X79EoFceO-SbRwqaxMIiKh2-bi7UDy9oPAkMIs/embed?start=false&loop=false&delayms=3000",
-  "5. User Input" => "https://docs.google.com/presentation/d/1GvTyOO8beomJO9bhowRGt_j4mcS6ST5CJzTy0LJaoJk/embed?start=false&loop=false&delayms=3000",
-  "6. Arrays" => "https://docs.google.com/presentation/d/1aYTknabRZQ_zC7WrRZGBuEcyWZnwWVn3U-woGdk95gU/embed?start=false&loop=false&delayms=3000",
-  "7. Functions" => "https://docs.google.com/presentation/d/1g7PdCyNzY0JmKIfQVLkhFpQ1VGIVswwydAXNqtZumuk/embed?start=false&loop=false&delayms=3000",
+  "0. Intro to JS Fiddle" => "https://docs.google.com/presentation/d/170XzoPLJkZcYjhpHoz5lFDymTsjKa9-rFK0CPi0-wmI",
+  "1. Variables" => "https://docs.google.com/presentation/d/1yFbB36iDzILW6oibqwO8ZA44keUjIk40flAsR0c77P0",
+  "2. Conditionals" => "https://docs.google.com/presentation/d/1DXWCMmM1LPjFbFcQ_yi55zyUUUjQnZiIF23oaSndnPQ",
+  "3. While Loops" => "https://docs.google.com/presentation/d/1n-QKn0nCXn_O-yz7fe_0_Y9eda2XApKQpEs9Om3YKG4",
+  "4. For Loops" => "https://docs.google.com/presentation/d/1Hzf15X79EoFceO-SbRwqaxMIiKh2-bi7UDy9oPAkMIs",
+  "5. User Input" => "https://docs.google.com/presentation/d/1GvTyOO8beomJO9bhowRGt_j4mcS6ST5CJzTy0LJaoJk",
+  "6. Arrays" => "https://docs.google.com/presentation/d/1aYTknabRZQ_zC7WrRZGBuEcyWZnwWVn3U-woGdk95gU",
+  "7. Functions" => "https://docs.google.com/presentation/d/1g7PdCyNzY0JmKIfQVLkhFpQ1VGIVswwydAXNqtZumuk",
 ];
 ?>
 
@@ -24,7 +24,7 @@ $a = [
       </div>
       <div class="modal-body">
           <div class="embed-responsive embed-responsive-16by9">
-            <iframe class="embed-responsive-item" src="<?php echo $value;?>" allowfullscreen></iframe>
+            <iframe class="embed-responsive-item" src="<?php echo $value;?>/embed?start=false&loop=false&delayms=3000" allowfullscreen></iframe>
           </div>
       </div>
       <div class="modal-footer">
